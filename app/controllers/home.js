b53c83bd8eaea4a03421c39661cc5840cec62516
Sanath Kumar Ramesh <sanath@goranku.com>
2015-03-12 18:35:32 -0700

    adding search page placeholder route & view

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -12,3 +12,27 @@ router.get('/', function (req, res, next) {
       articles: []
     });
 });
+
+router.get('/search', function (req, res, next) {
+    var hikes = [
+        {
+            name: "Hike 1",
+            length: 6.5, //miles
+            location: [0.123, -123.23], // lat, lon
+            elevGain: 1000, //feet
+            elevMax: 2000, //feet
+            desc: "description",
+            imgurl: "http://whatever",
+            drivingDirections: "How to get ther?",
+            popularity: 3,
+            rating: 2.5,
+            difficulty: 4,
+            drivingDistance: 80.2, //miles
+            drivingTime: 120, //minutes
+        }
+    ];
+
+    res.render('search', {
+        hikes: hikes
+    });
+});
