0d95ba2afde6cffd7c70678e60a02296b36f3314
Tito Soto <tito@createlabs.us>
2015-03-19 09:16:01 -0700

    Opening Hike UX

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,37 @@
 $(document).ready(function() {
-	
-	// expand hike on click to show description
-	$('.hike').on('click', function() {
-	  return $(this).toggleClass('open');
-	});
 
+	// animation to search for hikes
 	$('main [type=submit]').on('click', function() {
 		event.preventDefault();
 	  $('main').addClass('searching');
+	});
+
+	// back button to search again
+	$('#back').on('click', function() {
+	  $('main').removeClass('searching');
+	});
+
+	// expand hike on click to show description
+	$('.hike').on('click', function() {
+
+		// do not open if its already open
+		if ( $(this).hasClass('open')) {
+			return false;
+		}
+
+		// collapse other hikes
+		$('.hike').removeClass('open');
+		$('.hike .content').slideUp(500);
+
+		// show clicked hike
+		$('.content', this).slideDown(500);
+		$(this).addClass('open');
+
+		// // scroll to clicked hike
+		// $('html, body').animate({
+	 //        scrollTop: $(".hike.open").offset().top - 48
+	 //    }, 1000);
+
 	});
 
 });
